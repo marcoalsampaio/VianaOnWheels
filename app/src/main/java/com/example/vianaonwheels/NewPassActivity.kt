11661c6b029b6c74067ed6c83d7a3f997b8a5bd0
Marco <marcoalsampaio@gmail.com>
2022-04-30 15:42:35 +0100

    New pass Faltam animaçoes

--- app/src/main/java/com/example/vianaonwheels/NewPassActivity.kt
+++ app/src/main/java/com/example/vianaonwheels/NewPassActivity.kt
@@ -3,9 +3,9 @@ package com.example.vianaonwheels
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewPassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_pass)
     }
 }
