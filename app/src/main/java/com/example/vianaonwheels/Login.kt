4124eb68758bbf3c0f0d2d226bb6f223a2e346be
Marco <marcoalsampaio@gmail.com>
2022-04-30 17:33:11 +0100

    editTexts

--- app/src/main/java/com/example/vianaonwheels/Login.kt
+++ app/src/main/java/com/example/vianaonwheels/Login.kt
@@ -1,11 +1,15 @@
 package com.example.vianaonwheels
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Login : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 }
+
