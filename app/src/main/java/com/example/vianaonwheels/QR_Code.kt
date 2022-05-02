eae9c412d1ae81a78e4f31804438cddbfb41cc02
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-05-02 12:02:37 +0100

    QR Code Static Page

---
+++ app/src/main/java/com/example/vianaonwheels/QR_Code.kt
@@ -0,0 +1,11 @@
+package com.example.vianaonwheels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QR_Code : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code)
+    }
+}
