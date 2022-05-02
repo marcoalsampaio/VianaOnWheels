0b68b77e4b939824caf4bf072a97f00183b65169
BloodStreaam <47630797+BloodStreaam@users.noreply.github.com>
2022-05-02 15:41:42 +0100

    Historic Complete with filter icon in action bar

--- app/src/main/java/com/example/vianaonwheels/MainActivity.kt
+++ app/src/main/java/com/example/vianaonwheels/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.vianaonwheels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.hc_menu, menu);
+        return true;
     }
 }
