7b94d564b7d4102a1a68bd6826107111e5545916
hugocosta18 <82047791+hugocosta18@users.noreply.github.com>
2022-05-07 21:13:03 +0100

    pagina inicial

---
+++ app/src/main/java/com/example/vianaonwheels/Main_Page.kt
@@ -0,0 +1,11 @@
+package com.example.vianaonwheels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
