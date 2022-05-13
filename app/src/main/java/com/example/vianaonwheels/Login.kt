a8dc3deaea46a59525ee76aa0728b345b3eba0c1
Marco <marcoalsampaio@gmail.com>
2022-05-13 18:25:22 +0100

    Requireds

--- app/src/main/java/com/example/vianaonwheels/Login.kt
+++ app/src/main/java/com/example/vianaonwheels/Login.kt
@@ -16,36 +16,61 @@ import java.util.*
 class Login : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore;
 
+    private lateinit var email: EditText
+    private lateinit var pass: EditText
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         db= FirebaseFirestore.getInstance();
+        email = findViewById(R.id.edt_insert_email)
+        pass = findViewById(R.id.edt_insert_pass)
     }
 
     fun login(view: View) {
-        val email = findViewById<EditText>(R.id.edt_insert_email).text.toString()
-        val pass = findViewById<EditText>(R.id.edt_insert_pass).text.toString() //Encriptar dps!!! ?? se tiver tempo
-        Log.d(TAG,email)
-        if (email.isNotEmpty() && pass.isNotEmpty()){
-                db.collection("Cliente")
-                    .whereEqualTo("Email", email)
-                    .whereEqualTo("Password", pass)
-                    .get()
-                    .addOnCompleteListener{ task ->
-                        if(task.isSuccessful){
-                            for (d in task.result!!){
-                                Log.d(TAG,d.id) //Ver Resto!!!!! Passsar ID para proxima atividade
-                            }
+         //Encriptar dps!!! ?? se tiver tempo
+        if(checkAllFieldsInput()){
+            db.collection("Cliente")
+                .whereEqualTo("Email", email.text.toString())
+                .whereEqualTo("Password", pass.text.toString())
+                .get()
+                .addOnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        for (d in task.result!!){
+                            Log.d(TAG,d.id) //Ver Resto!!!!! Passsar ID para proxima atividade
                         }
-                    }.addOnFailureListener {
-                        Log.w(TAG, "", it)
+                        email.error = "Wrong Credentials";
+                        pass.error = "Wrong Credentials";
+                    }else{
+                        Toast.makeText(this, "Try later", Toast.LENGTH_LONG)
                     }
-        }else{
-            //Mensagem Inserir daods corretos
-            Toast.makeText(this, "Insira os dados corretos", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Try later", Toast.LENGTH_LONG)
+                }
         }
     }
+    private fun  checkAllFieldsInput(): Boolean { //Strings
+
+        if (email.length() == 0 && pass.length() == 0){
+            email.error = "Email is required";
+            pass.error = "Password is required";
+            return false;
+        }
+        if (email.length() == 0){
+            email.error = "Email is required";
+            return false;
+        }
+
+        if (pass.length() == 0){
+            pass.error = "Password is required";
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
+    }
+
 
     //Receber dados
     //conectar a base de dados
