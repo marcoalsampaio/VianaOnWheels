ed1efc38afd279f5b0459ece3bcf0723f3c57940
marcoalsampaio <marcoalsampaio@gmail.com>
2022-05-19 15:51:05 +0100

    Revert "Pronto para merge"
    This reverts commit e74780e4

---
+++ app/src/main/java/com/example/vianaonwheels/Horarios.kt
@@ -0,0 +1,11 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vianaonwheels.R
+
+class Horarios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horarios)
+    }
+
+}
