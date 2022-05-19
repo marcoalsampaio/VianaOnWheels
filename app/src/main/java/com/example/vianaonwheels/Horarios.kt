90ab545cb624728a39b23f5352b18742f7adf907
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-05-19 15:17:08 +0100

    Revert "Merge branch 'aboutUs'"
    This reverts commit 96a5f77b6e0eb676184abb4ca82a987893c06a7e, reversing
    changes made to f7bf506d5d656ae643f7b46ca4deed5630913c88.

--- app/src/main/java/com/example/vianaonwheels/Horarios.kt
+++ app/src/main/java/com/example/vianaonwheels/Horarios.kt
@@ -3,16 +3,9 @@ package com.example.vianaonwheels
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-<<<<<<< HEAD:app/src/main/java/com/example/vianaonwheels/Horarios.kt
 class Horarios : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_horarios)
-=======
-class AboutUs : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_us)
->>>>>>> aboutUs:app/src/main/java/com/example/vianaonwheels/AboutUs.kt
     }
 }
