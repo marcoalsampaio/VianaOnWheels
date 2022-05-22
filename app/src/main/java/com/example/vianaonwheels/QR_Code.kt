61f43eaec7eb20df8430d9ae5d0da21d5ad1824d
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-05-22 20:15:46 +0100

    Generate Qr Code from String

--- app/src/main/java/com/example/vianaonwheels/QR_Code.kt
+++ app/src/main/java/com/example/vianaonwheels/QR_Code.kt
@@ -1,11 +1,36 @@
 package com.example.vianaonwheels
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeWriter
 
 class QR_Code : AppCompatActivity() {
+    private var imageView: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code)
+
+        imageView = findViewById(R.id.imageView)
+        val qrCodeWriter = QRCodeWriter()
+        try {
+            val bitMatrix: BitMatrix =
+                qrCodeWriter.encode("editText!!", BarcodeFormat.QR_CODE, 200, 200)
+            val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.RGB_565)
+            for (x in 0..199) {
+                for (y in 0..199) {
+                    bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+                }
+            }
+            imageView!!.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 }
