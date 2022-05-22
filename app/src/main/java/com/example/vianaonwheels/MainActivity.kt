68a975b8d945c61fb4f80f8adaf33931bc1ba32f
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-05-22 20:05:50 +0100

    Load Infomation of Ticket

--- app/src/main/java/com/example/vianaonwheels/MainActivity.kt
+++ app/src/main/java/com/example/vianaonwheels/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.vianaonwheels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  cardBuy : TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cardBuy=findViewById(R.id.cardBuy)
+        cardBuy.text="Partida:" + getString(R.string.tab) + "18:30"+ "-" + "Maia"+ "\nChegada:"+ getString(R.string.tab)  + "18:30"+ "-" + "Maia" + "\nPreço:"+ getString(R.string.tab)  + "2,55€"
+    }
+
+    fun buyTicket(view: View) {
+
     }
 }
