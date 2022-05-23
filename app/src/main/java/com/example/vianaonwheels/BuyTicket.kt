de0a00f1335140ff37b1f7b03decff690699246a
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-05-23 21:26:25 +0100

    BuyTicket - pronto para merge

--- app/src/main/java/com/example/vianaonwheels/BuyTicket.kt
+++ app/src/main/java/com/example/vianaonwheels/BuyTicket.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class BuyTicket : AppCompatActivity() {
     private lateinit var  cardBuy : TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_buyticket)
 
         cardBuy=findViewById(R.id.cardBuy)
         cardBuy.text="Partida:" + getString(R.string.tab) + "18:30"+ "-" + "Maia"+ "\nChegada:"+ getString(R.string.tab)  + "18:30"+ "-" + "Maia" + "\nPreço:"+ getString(R.string.tab)  + "2,55€"
