05be41daec646fc849400fecf0fb189067e0ee31
marcoalsampaio <marcoalsampaio@gmail.com>
2022-05-24 16:08:58 +0100

    Splash screan Logo

---
+++ app/src/main/java/com/example/vianaonwheels/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vianaonwheels
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, Login::class.java))
+        finish()
+    }
+}
