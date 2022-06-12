df486cbb02660e4e8ac9ba526c094ccaac1447df
vitoRibeiro99 <79645242+vitoRibeiro99@users.noreply.github.com>
2022-06-12 15:02:20 +0100

    Top Bar no QRScanner

--- app/src/main/java/com/example/vianaonwheels/QR_scanner.kt
+++ app/src/main/java/com/example/vianaonwheels/QR_scanner.kt
@@ -1,19 +1,42 @@
 package com.example.vianaonwheels
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.budiyev.android.codescanner.*
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class QR_scanner : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
     private val PERMISSION_REQUEST_CODE = 200
+    private lateinit var userEmail : String
+    //TopBar
+    private lateinit var nDrawerLayout: DrawerLayout;
+    private lateinit var navView: NavigationView;
+    private lateinit var tituloPagina: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scanner)
+
+        nDrawerLayout = findViewById(R.id.drawerLayout)
+        navView= findViewById(R.id.navView)
+
+        tituloPagina= findViewById(R.id.tituloPagina)
+        tituloPagina.setText(R.string.scanner)
+        userEmail = intent.getStringExtra(EXTRA_USEREMAIL).toString()
+
         requestPermission()
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
 
@@ -59,5 +82,52 @@ class QR_scanner : AppCompatActivity() {
     override fun onPause() {
         codeScanner.releaseResources()
         super.onPause()
+    }
+    fun aboutUS(view: View) {
+        val intent = Intent(this, AboutUsActivity::class.java).apply {
+            putExtra(EXTRA_USEREMAIL, userEmail)
+        }
+        startActivity(intent)
+        overridePendingTransition(R.anim.out_in,R.anim.in_out)
+    }
+    fun deleteAcc(view: View) {
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        db= FirebaseFirestore.getInstance()
+        db.collection("User").document(userID).delete()
+            .addOnSuccessListener { Log.d(ContentValues.TAG, getString(R.string.account_deleted))
+                //Mensagem após Eliminar Conta
+                dialogBuilder.setPositiveButton(/*getString(R.string.ok)*/"OK") { _, _ ->
+                    //Redirect to Login
+                    val intent = Intent(this, Login::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.out_in,R.anim.in_out)
+                }
+                // create dialog box
+                val alert = dialogBuilder.create()
+                // set title for alert dialog box
+                alert.setTitle(getString(R.string.account_deleted))
+                // show alert dialog
+                alert.show()
+            }
+            .addOnFailureListener { e -> Log.w(ContentValues.TAG, getString(R.string.error_deleting), e)
+                Toast.makeText(this,  getString(R.string.error_deleting), Toast.LENGTH_LONG).show()}
+    }
+    fun logout(view: View) {
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent)
+        overridePendingTransition(R.anim.out_in,R.anim.in_out)
+        finish()
+    }
+    fun backIcon(view: View) {
+        val intent = Intent(this, MainPage::class.java).apply {
+            putExtra(EXTRA_USEREMAIL, userEmail)
+        }
+        startActivity(intent)
+        overridePendingTransition(R.anim.out_in,R.anim.in_out) }
+
+    fun menuIcon(view: View) {
+        nDrawerLayout.openDrawer(navView)
     }
 }
